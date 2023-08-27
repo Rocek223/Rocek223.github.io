ef8406572dba2a18d847f7da260d786b1e0aa6f0
rocek223 <rocek2232000@gmail.com>
2023-08-27 21:24:19 +0200

    odesílání obrázků na server

--- GameChoice/Games/gameOptions.js
+++ GameChoice/Games/gameOptions.js
@@ -9,7 +9,6 @@ function onCustomIconChange(inputFileId, iconPreviewId) {
 function onCustomTextureChange(inputFileId, iconPreviewId) {
   const [file] = document.getElementById(inputFileId).files
   if (file) {
-    console.log("69");
     document.getElementById(iconPreviewId).src = URL.createObjectURL(file)
   }
 }
@@ -40,21 +39,17 @@ function onPlatformSelected() {
   }
 }
 function onCustomOptionChange(selectionId, iconPreviewId, option1, option2, option3){
-  console.log("cum");
   var select = document.getElementById(selectionId);
   var platformIcon = document.getElementById(iconPreviewId);
 
   if (select.value == 1){
     platformIcon.src = option1;
-    console.log(platformIcon);
 
   }else if(select.value == 2){
     platformIcon.src = option2;
-    console.log(platformIcon);
   }else
   {
     platformIcon.src = option3;
-    console.log(platformIcon);
   }
 }
 function onCustomSoundOptionChange(selectionId, audioPreviewId, audioSourcePreviewId, option1, option2, option3){
@@ -81,11 +76,61 @@ function onCustomSoundChange(inputFileId, audioSourcePreviewId, audioPreviewId) 
   if (file) {
     
     document.getElementById(audioSourcePreviewId).src = URL.createObjectURL(file)
-    console.log("buzn");
   }
   audioPreview.load();
 }
 
 function sendEmail() {
-  // TODO: Send email ._.
+  /*$.get(
+      "http://10.0.0.114:5000/send",
+      {name : "smazat", data : "nic"},
+      function(data) {
+         alert('page content: ' + data);
+      }
+  );*/
+  
+  const images = document.getElementsByName("inputImage");
+  const imagesURL = [];
+
+  for (let i = 0; i < images.length; i++) {
+    imagesURL[i] = getBase64Image(images[i]);
+    $.get(
+        "http://10.0.0.114:5000/send",
+        {name : images[i].id, data : imagesURL[i]},
+        function(data) {
+           alert('page content: ' + data);
+        }
+    );
+  }
+  console.log(imagesURL);
+
+
 }
+function getInfo(item, index){
+  item.id + ":  " + item.value + "<br>";
+}
+
+function getBase64Image(img) {
+    img.crossOrigin="anonymous"
+    // Create an empty canvas element
+    var canvas = document.createElement("canvas");    
+
+    var dividerW = img.naturalWidth > 1000 ? 4 : img.naturalWidth < 100 ? 1 : 2;
+    var dividerH = img.naturalHeight > 1000 ? 4 : img.naturalHeight < 100 ? 1 : 2;
+
+    canvas.width = img.naturalWidth / dividerW;
+    canvas.height = img.naturalHeight / dividerH;
+
+    // Copy the image contents to the canvas
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
+
+    // Get the data-URL formatted image
+    // Firefox supports PNG and JPEG. You could check img.src to
+    // guess the original format, but be aware the using "image/jpg"
+    // will re-encode the image.
+    var dataURL = canvas.toDataURL("image/jpeg");
+
+    return dataURL.replace(/^data:image\/(png|jpg|jpeg);base64,/, "");
+}
+
